4051ecab3cfdf1da1f20348b7efcb4124261564e
Joe Goggins <joe.goggins@gmail.com>
2014-01-16 16:53:44 -0600

    added moment.js and flot.js and fixed exported env vars in start_server.sh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,16 @@
 //= require jquery_ujs
 //= require jquery.knob
 //= require jquery.form
+//
+// Visualization Stuff
+//= require moment.min
+//= require jquery.flot
+// 
+// NOTE: INCLUDE WHATEVER YOU NEED HERE
+// jquery.flot.canvas.js		jquery.flot.js			jquery.flot.symbol.js
+// jquery.flot.categories.js	jquery.flot.navigate.js		jquery.flot.threshold.js
+// jquery.flot.crosshair.js	jquery.flot.pie.js		jquery.flot.time.js
+// jquery.flot.errorbars.js	jquery.flot.resize.js		jquery.form.js
+// jquery.flot.fillbetween.js	jquery.flot.selection.js	jquery.knob.js
+// jquery.flot.image.js		jquery.flot.stack.js		moment.min.js
 //= require_tree .
