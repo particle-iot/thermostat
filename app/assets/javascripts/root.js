09f6436ef93764681052c9574e9b4aad7930e2f3
David Middlecamp <david@spark.io>
2014-01-16 17:06:16 -0600

    refactor into a class for "this"

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,15 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-// On Document Ready: Bind things to the
-$(function() {
-  $("#actual_temp_change_form .dial").knob({});
-  $("#desired_temp_change_form .dial").knob({
-    'release' : function (v) {
-      console.info(v);
-      var form = $('#desired_temp_change_form').ajaxSubmit({ /* options */ });
-      var xhr = form.data('jqxhr');
-      xhr.done(function() {});
+var Page = function () {
+};
+Page.prototype = {
+    startup: function () {
+        $("#actual_temp_change_form .dial").knob({});
+        $("#desired_temp_change_form .dial").knob({
+            'release': function (v) {
+                console.info(v);
+                var form = $('#desired_temp_change_form').ajaxSubmit({ /* options */ });
+                var xhr = form.data('jqxhr');
+                xhr.done(function () {
+                });
+            }
+        });
     }
-  })
-});
+};
+var page = new Page();
+
+// On Document Ready: Bind things to the
+$(page.startup);
+
+
+
