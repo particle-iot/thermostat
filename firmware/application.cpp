8ad0fbba3cc2f359ec2701faf04bf10b7d539acd
Zachary Crockett <towynlin@gmail.com>
2014-01-16 13:47:57 -0600

    Working LED matrix

--- firmware/application.cpp
+++ firmware/application.cpp
@@ -1,9 +1,34 @@
+#include "application.h"
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_8x8matrix matrix1;
+Adafruit_8x8matrix matrix2;
+Adafruit_8x8matrix matrix3;
+
+static const uint8_t smile[] = {
+  0b00111100,
+  0b01000010,
+  0b10100101,
+  0b10000001,
+  0b10100101,
+  0b10011001,
+  0b01000010,
+  0b00111100
+};
+
 void setup()
 {
-
+  Wire.begin();
+  matrix1.begin(0x70);
+  matrix2.begin(0x71);
+  matrix3.begin(0x72);
 }
 
 void loop()
 {
-
+  matrix1.clear();
+  delay(500);
+  matrix1.drawBitmap(0, 0, smile, 8, 8, LED_ON);
+  matrix1.writeDisplay();
+  delay(500);
 }
