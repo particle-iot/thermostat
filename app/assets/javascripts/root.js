0e5639d5e8de0f371d4772fa21b4bf51bf174a0c
David Middlecamp <david@spark.io>
2014-01-16 17:14:27 -0600

    stubbing in a graph with fake data / goofy styling

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -12,15 +12,66 @@ Page.prototype = {
                 var form = $('#desired_temp_change_form').ajaxSubmit({ /* options */ });
                 var xhr = form.data('jqxhr');
                 xhr.done(function () {
+                    console.log("form submitted")
                 });
             }
         });
+
+        this.renderGraph();
+    },
+
+
+    pollServer: function () {
+
+    },
+
+
+    renderGraph: function () {
+
+        //fake data
+        var sin = [],
+            cos = [];
+
+        for (var i = 0; i < 14; i += 0.5) {
+            sin.push([i, Math.sin(i)]);
+            cos.push([i, Math.cos(i)]);
+        }
+
+        var plot = $.plot("#realtime_graph", [
+            { data: sin, label: "sin(x)"},
+            { data: cos, label: "cos(x)"}
+        ], {
+            series: {
+                lines: {
+                    show: true
+                },
+                points: {
+                    show: true
+                }
+            },
+            grid: {
+                hoverable: true,
+                clickable: true
+            },
+            yaxis: {
+                min: -1.2,
+                max: 1.2
+            }
+        });
+
+    },
+
+    updateGraph: function () {
+
     }
+
+
 };
 var page = new Page();
 
 // On Document Ready: Bind things to the
-$(page.startup);
+$(page.startup.bind(page));
+
 
 
 
